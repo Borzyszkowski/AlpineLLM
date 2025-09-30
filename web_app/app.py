567e875a4081aaea1c53ad4a9a1954859496e6de
Borzyszkowski <bartek_borzyszkowski@wp.pl>
2025-10-01 00:48:31 +0200

    ALP-6: first web app working

--- web_app/app.py
+++ web_app/app.py
@@ -1,43 +1,20 @@
-""" A simple Gradio web app to interact with the AlpineLLM model. """
+""" A simple Gradio web app to interact with the AlpineLLM model """
 
 import gradio as gr
+import os
 import torch
 
 from demo_inference import AlpineLLMInference
-from tokenizer import CharacterLevelTokenizer
-
-
-def initialize_inference():
-    """ Load tokenizer and model """
-
-    # Load tokenizer
-    tokenizer = CharacterLevelTokenizer()
-
-    # Load model (re-instantiate + load weights)
-    inference = AlpineLLMInference(
-        cfg={"load_weights_path": "model/best_model"},
-        hyperparam_cfg={
-            "embedding_dim": 384,
-            "num_heads": 6,
-            "num_layers": 6,
-            "dropout": 0.2,
-            "context_len": 256,
-            "lr": 3e-4,
-        },
-        tokenizer=tokenizer,
-    )
-
-def generate(prompt, max_tokens=200):
-    return inference.generate_text(prompt, max_new_tokens=max_tokens)
+from config_util import Config
 
 
 def start_app():
     """ Start the web app via Gradio """
     app = gr.Interface(
-        fn=generate,
+        fn=inference.generate_text,
         inputs=[
             gr.Textbox(lines=3, placeholder="Type your alpinism prompt..."),
-            gr.Slider(50, 500, value=200, step=10, label="Max tokens"),
+            gr.Slider(50, 1000, value=200, step=10, label="Max tokens"),
         ],
         outputs=gr.Textbox(),
         title="AlpineLLM Demo",
@@ -47,4 +24,27 @@ def start_app():
 
 
 if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # Define the configuration
+    cfg = {
+        'load_weights_path': "model/best_model",
+        'cuda_id': 0,
+        'model_type': 'transformer',
+    }
+    cfg = Config(cfg)
+
+    # Define the hyperparameters
+    hyperparam_cfg={
+        "embedding_dim": 384,
+        "num_heads": 6,
+        "num_layers": 6,
+        "dropout": 0.2,
+        "context_len": 256,
+        "lr": 3e-4,
+    }
+    hyperparam_cfg = Config(hyperparam_cfg)
+
+    # Start the application
+    inference = AlpineLLMInference(cfg, hyperparam_cfg)
     start_app()
